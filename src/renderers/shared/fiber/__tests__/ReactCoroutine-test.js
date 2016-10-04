kirillalazzekh/WPF_App

I wrote it live! (#7663)

src/renderers/shared/fiber/__tests__/ReactCoroutine-test.js
@@ -15,14 +15,14 @@ var React;
 var ReactNoop;
 var ReactCoroutine;
 
-describe('ReactCoroutine', function() {
-  beforeEach(function() {
+describe('ReactCoroutine', () => {
+  beforeEach(() => {
     React = require('React');
     ReactNoop = require('ReactNoop');
     ReactCoroutine = require('ReactCoroutine');
   });
 
-  it('should render a coroutine', function() {
+  it('should render a coroutine', () => {
 
     var ops = [];
 

mrizwanashiq/react

(cherry picked from commit 334b8bdf165c47336b467a4092e3f51893a83c0d)

src/renderers/shared/fiber/__tests__/ReactCoroutine-test.js
@@ -15,14 +15,14 @@ var React;
 var ReactNoop;
 var ReactCoroutine;
 
-describe('ReactCoroutine', function() {
-  beforeEach(function() {
+describe('ReactCoroutine', () => {
+  beforeEach(() => {
     React = require('React');
     ReactNoop = require('ReactNoop');
     ReactCoroutine = require('ReactCoroutine');
   });
 
-  xit('should render a coroutine', function() {
+  xit('should render a coroutine', () => {
 
     var ops = [];
 